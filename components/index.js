85a7b3babc3a0df5f60770ba3fb21b22e8063ca1
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-09-27 14:31:25 -0400

    Create index.js

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import ExtendedHeading from "./ExtendedHeading";
+import Layout from "./Layout";
+import Message from "./Message";
+import MessageInput from "./MessageInput";
+import TrashIcon from "./TrashIcon";
+import Hero from "./Hero";
+import Heading from "./Heading";
+
+
+
+export { ExtendedHeading, Layout, Message, Heading, MessageInput, TrashIcon, Hero }
