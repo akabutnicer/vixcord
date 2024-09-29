e79d9082587c810953dd0cfe5533c2fe9202858e
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-09-28 20:10:11 -0400

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -79,25 +79,33 @@ const Login = () => {
             <label className="font-bold text-grey-darker block mb-2">
               Email
             </label>
-            <input
-              type="email"
-              className="block bg-[#e59cff] relative w-full focus:outline-none outline-none border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-              placeholder="Enter your email"
-              value={password}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+            <div class="max-w-[395px] space-x-2 space-x-reverse h-12 rounded-lg flex justify-center items-center w-full border border-white/[0.12] bg-white/[0.05]">
+              <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Enter your Email"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                class=" rounded-lg bg-transparent w-full py-1 px-6 outline-none"
+              />
+            </div>
           </div>
           <div className="mb-4">
             <label className="font-bold text-grey-darker block mb-2">
               Password
             </label>
-            <input
-              type="password"
-              className="block relative bg-[#e59cff] w-full focus:outline-none outline-none border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-              placeholder="Your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <div class="max-w-[395px] space-x-2 space-x-reverse h-12 rounded-lg flex justify-center items-center w-full border border-white/[0.12] bg-white/[0.05]">
+              <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Enter your Email"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                class=" rounded-lg bg-transparent w-full py-1 px-6 outline-none"
+              />
+            </div>
           </div>
 
           <div className="flex flex-col gap-2">
@@ -111,15 +119,18 @@ const Login = () => {
             >
               Login
             </a>
-            <a
-              href="/signup"
-              onclick="void(0)"
-              class="relative hover:text-decoration:underline"
-            >
-              <span class="hero-subtitle-text">
-                Your simple, fun, and sleek communication companion!{" "}
-              </span>
-            </a>
+            <span>
+                <span class="text-gray">Don't have an account?</span>
+              <a
+                href="/signup"
+                onclick="void(0)"
+                class="relative hover:underline p-0"
+              >
+                <span class="hero-subtitle-text">
+                  Register
+                </span>
+              </a>
+            </span>
           </div>
         </div>
       </div>
@@ -128,3 +139,4 @@ const Login = () => {
 };
 
 export default Login;
+x
